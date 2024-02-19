01d103f8b5ddc7c446a6708ceffc7c013405d812
Oliverdddddddd <1347091498@qq.com>
2024-02-19 12:37:22 +0100

    n

--- youbot_ICNN_publisher.py
+++ youbot_ICNN_publisher.py
@@ -6,14 +6,15 @@ import torch
 from std_msgs import Float64MultiArray
 from brics_actuator.msg import JointPositions,JointValue
 import numpy as np
-# class ICNNNetworkPolicy:
-# 	def __init__(self,model_path):
-# 		self.icnn_model = torch.load(model_path)
-# 		self.icnn_model.eval()
-# 	def generate_action(self, observation):
-# 		with torch.icnn_model(observation):
-# 			action = self.icnn_model(observation)
-# 		return action.numpy
+from sensor_msgs.msg import JointState
+class ICNNNetworkPolicy:
+	def __init__(self,model_path):
+		self.icnn_model = torch.load(model_path)
+		self.icnn_model.eval()
+	def generate_action(self, observation):
+		with torch.icnn_model(observation):
+			action = self.icnn_model(observation)
+		return action.numpy
 def youbot_joint_publisher():
 	rospy.init_node("youbot_joint_publisher")
 	joint_pub = rospy
@@ -21,5 +22,7 @@ def youbot_joint_publisher():
 def main():
 	rospy.init_node('icnn_policy_publisher')
 	rate = rospy.Rate(10)
+	model = torch.load("/home/oliver/rospy_youbot/new_policy.pth")
+	model.eval()
 	policy = ICNNNetworkPolicy("policy.pth")
 	pub = rospy.Publisher("")
